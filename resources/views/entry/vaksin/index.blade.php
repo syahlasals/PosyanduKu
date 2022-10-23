755a4d91a0832301b9206b28c57cf0fc4232ca10
Nabila A <nabilaalthaf712@gmail.com>
2022-10-23 09:35:06 +0700

    vv

--- resources/views/entry/vaksin/index.blade.php
+++ resources/views/entry/vaksin/index.blade.php
@@ -98,8 +98,8 @@
                                     </div>
                                 </div>
 
-                                {{-- <a href="{{ url('entry/vaksin') }}" class="btn btn-main" type="submit">Simpan</a> --}}
-                                {{-- <a href="{{ url('entry/InputImunisasi') }}" class="btn btn-main" role="button" aria-disabled="true">Kembali</a> --}}
+                                <!-- {{-- <a href="{{ url('entry/vaksin') }}" class="btn btn-main" type="submit">Simpan</a> --}} -->
+                                <!-- {{-- <a href="{{ url('entry/InputImunisasi') }}" class="btn btn-main" role="button" aria-disabled="true">Kembali</a> --}} -->
                             </form>
                         </div><br>
                     </div>
