1083d0e35364808fe2c159e6a853115451b906d2
wulansyantik28@gmail.com <wulansyantik28@gmail.com>
2022-12-04 19:35:05 +0700

    fix bug

--- resources/views/entry/statusPenyimpangan/index.blade.php
+++ resources/views/entry/statusPenyimpangan/index.blade.php
@@ -59,8 +59,7 @@
                                         </div>
                                         <div class="col-1"><b>:</b></div>
                                         <div class="col-8"> 
-                                            <input type="date" name="tgl_hitung" id="tgl_hitung" value="{{ $tgl_hitung->tgl_hitung }}" class="form-control" disabled>
-                                            <input type="hidden" name="tgl_hitung" id="tgl_hitung" value="{{ $tgl_hitung->tgl_hitung }}" class="form-control">
+                                            <input type="date" name="tgl_hitung" id="tgl_hitung" class="form-control">
                                         </div>
                                     </div>
                                     <div class="row" style="margin-bottom: 15px;">
