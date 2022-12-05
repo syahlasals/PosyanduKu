d8122dc880ad42cad55a1d9866bb633405d48866
syahlasals <syahlasbl28@gmail.com>
2022-12-05 08:08:09 +0700

    ubah controller imunisasi

--- app/Http/Controllers/ImunisasiController.php
+++ app/Http/Controllers/ImunisasiController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Anak;
 use App\Models\Imunisasi;
 use App\Models\Penyimpangan;
@@ -13,10 +14,10 @@ class ImunisasiController extends Controller
     {
         $anaks = Anak::all();
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.index', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -25,22 +26,21 @@ class ImunisasiController extends Controller
             'stunting' => $stunting,
             'obesitas' => $obesitas
         ]);
-        
     }
 
     public function normal()
     {
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Normal')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Normal')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.normal', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -50,20 +50,20 @@ class ImunisasiController extends Controller
             'obesitas' => $obesitas
         ]);
     }
-    
+
     public function giziBuruk()
     {
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Gizi Buruk')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Gizi Buruk')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.giziBuruk', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -77,16 +77,16 @@ class ImunisasiController extends Controller
     public function stunting()
     {
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Stunting')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Stunting')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.stunting', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -96,20 +96,22 @@ class ImunisasiController extends Controller
             'obesitas' => $obesitas
         ]);
     }
-    
+
     public function obesitas()
     {
-        $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
+        /* $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
                 ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
                 ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
                 ->where('tb_pertumbuhan.status_penyimpangan', 'Obesitas')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
-
+                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']); */
+        $anaks = Anak::wherehas('penyimpangans', function ($query) {
+            return $query->where('status_penyimpangan', 'Obesitas');
+        })->with(['penyimpangans', 'vaksins', 'vitamins', 'tumbuhs'])->get();
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.obesitas', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -125,10 +127,10 @@ class ImunisasiController extends Controller
         $tanggal = date('l, d F y');
         $anaks = Anak::all();
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.cetak.cetakSemua', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -136,7 +138,7 @@ class ImunisasiController extends Controller
             'giziBuruk' => $giziBuruk,
             'stunting' => $stunting,
             'obesitas' => $obesitas,
-            'tanggal' => $tanggalapp
+            'tanggal' => $tanggal
         ]);
     }
 
@@ -144,16 +146,16 @@ class ImunisasiController extends Controller
     {
         $tanggal = date('d F y');
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Normal')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Normal')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.cetak.cetakNormal', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -169,16 +171,16 @@ class ImunisasiController extends Controller
     {
         $tanggal = date('d F y');
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Gizi Buruk')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Gizi Buruk')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.cetak.cetakGiziBuruk', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -194,16 +196,16 @@ class ImunisasiController extends Controller
     {
         $tanggal = date('d F y');
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Stunting')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Stunting')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','Stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'Stunting')->count();
         return view('pengelola.dataImunisasi.cetak.cetakStunting', [
             'anakList' => $anaks,
             'semua' => $semua,
@@ -213,22 +215,21 @@ class ImunisasiController extends Controller
             'obesitas' => $obesitas,
             'tanggal' => $tanggal
         ]);
-
     }
     public function cetakObesitas()
     {
         $tanggal = date('d F y');
         $anaks = Anak::join('tb_vaksin', 'tb_vaksin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
-                ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
-                ->where('tb_pertumbuhan.status_penyimpangan', 'Obesitas')
-                ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
+            ->join('tb_vitamin', 'tb_vitamin.nik_anak', '=', 'tb_anak.nik_anak')
+            ->join('tb_pertumbuhan', 'tb_pertumbuhan.nik_anak', '=', 'tb_anak.nik_anak')
+            ->where('tb_pertumbuhan.status_penyimpangan', 'Obesitas')
+            ->get(['tb_anak.nik_anak', 'tb_anak.nama_anak', 'tb_vaksin.jenis_vaksin', 'tb_vaksin.tgl_vaksin', 'tb_vitamin.jenis_vitamin', 'tb_vitamin.tgl_vitamin', 'tb_pertumbuhan.status_penyimpangan']);
 
         $semua = Anak::count();
-        $normal = Penyimpangan::where('status_penyimpangan','Normal')->count();
-        $giziBuruk = Penyimpangan::where('status_penyimpangan','Gizi Buruk')->count();
+        $normal = Penyimpangan::where('status_penyimpangan', 'Normal')->count();
+        $giziBuruk = Penyimpangan::where('status_penyimpangan', 'Gizi Buruk')->count();
         $obesitas = Penyimpangan::where('status_penyimpangan', 'Obesitas')->count();
-        $stunting = Penyimpangan::where('status_penyimpangan','stunting')->count();
+        $stunting = Penyimpangan::where('status_penyimpangan', 'stunting')->count();
         return view('pengelola.dataImunisasi.cetak.cetakObesitas', [
             'anakList' => $anaks,
             'semua' => $semua,
