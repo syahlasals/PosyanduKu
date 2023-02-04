dbbf8a0b1e18f4d5a7de2849fff1df3f3d5f38ae
Lastri772 <laswlndr11@gmail.com>
2023-02-04 14:31:30 +0700

    bug_rekap

--- resources/views/template/imun.blade.php
+++ resources/views/template/imun.blade.php
@@ -22,7 +22,6 @@
                 @include('template.nav')
                 <!--End of Topbar-->
 
-<<<<<<< HEAD
         <div class="container-fluid">
                     <h1 class="text-center mt-5 mb-5"><b>REKAP ANAK</b></h1>
                     <div>
@@ -99,74 +98,5 @@
                             </th>
                         </tr>
                     </table>
-=======
-        
-
-        <div class="row">
-            <div class="col">
-                <a href="{{ url('pengelola/dataImunisasi') }}" class="card card-ap">
-                <div class="card-body">
-                    <span class=" d-flex align-items-center justify-content-center mb-3 text-center"><b>Jumlah Anak <br> Terdaftar</b></span> 
-                    <h1 class="text-center">{{ $semua }}</h1>
-                </div>
-                </a>
-            </div>
-            <div class="col">
-                <a href="{{ url('pengelola/dataImunisasi/normal') }}" class="card card-ip">
-                <div class="card-body">
-                    <span class=" d-flex align-items-center justify-content-center mb-3 text-center"><b>Jumlah Anak <br> Normal</b></span> 
-                    <h1 class="text-center">{{ count($normal) }}</h1>
-                </div>
-                </a>
-            </div>
-            <div class="col">
-                <a href="{{ url('pengelola/dataImunisasi/giziBuruk') }}" class="card card-ap">
-                <div class="card-body">
-                    <span class=" d-flex align-items-center justify-content-center mb-3 text-center"><b>Jumlah Anak <br> Gizi Buruk</b></span> 
-                    <h1 class="text-center">{{ count($giziBuruk) }}</h1>
-                </div>
-                </a>
-            </div>
-            <div class="col">
-                <a href="{{ url('pengelola/dataImunisasi/obesitas') }}" class="card card-ip">
-                <div class="card-body">
-                    <span class=" d-flex align-items-center justify-content-center mb-3 text-center"><b>Jumlah Anak <br> Obesitas</b></span> 
-                    <h1 class="text-center">{{ count($obesitas) }}</h1>
-                </div>
-                </a>
-            </div>
-            <div class="col">
-                <a href="{{ url('pengelola/dataImunisasi/stunting') }}" class="card card-ap">
-                <div class="card-body">
-                    <span class=" d-flex align-items-center justify-content-center mb-3 text-center"><b>Jumlah Anak <br> Stunting</b></span> 
-                    <h1 class="text-center">{{ count($stunting) }}</h1>
-                </div>
-                </a>
-            </div>
-        </div>
-<br>
-
-        <table class="table table-bordered text-center">
-            <tr>
-                <th>
-                    <a class="table-filter" href="{{ url('pengelola/dataImunisasi/notvaksin') }}">
-                            <span class=" d-flex align-items-center justify-content-center mb-3 text-center">Anak Yang Belum Dapat Vaksin : {{ count($notvaksin) }} </span>
-                    </a>
-                </th>
-                <th>
-                    <a class="table-filter" href="{{ url('pengelola/dataImunisasi/notvitamin') }}" >
-                            <span class=" d-flex align-items-center justify-content-center mb-3 text-center">Anak Yang Belum Dapat Vitamin : {{ count($notvitamin) }}</span> 
-                    </a>
-                </th>
-                <th>
-                    <a class="table-filter" href="{{ url('pengelola/dataImunisasi/notkondisi') }}" >
-                            <span class=" d-flex align-items-center justify-content-center mb-3 text-center">Anak Yang Belum Terdeteksi Kelainan  : {{ count($notkondisi) }}</span> 
-                    </a>
-                </th>
-            </tr>
-        </table>
-
-        <br>
->>>>>>> a39e66a0bcfb9e7e4d7f5ef96853d69cb3eb1736
 
         {{-- isi disinii --}}
